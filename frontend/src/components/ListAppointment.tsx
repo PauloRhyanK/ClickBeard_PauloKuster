9dc81184b3ae374401ee5da7171166d4f3b2ca54
Paulo Kuster <paulorhyank@gmail.com>
2025-07-21 21:44:10 -0300

    feat: client and admin dashboard style

--- frontend/src/components/ListAppointment.tsx
+++ frontend/src/components/ListAppointment.tsx
@@ -1,67 +1,142 @@
 const ListAppointment = ({ role }: { role: string }) => {
-    alert("ListAppointment component loaded with role: " + role);
     if (role === "client") {
         return (
-        <div>
-            <section className="pr-10 pl-20">
-                <div className="flex flex-col items-start pt-20 pb-10">
-                    <h2 className="title-lg">Seus agendamentos</h2>
-                    <p className="p-sm subt-color">Aqui você pode visualizar seus horários marcados.</p>
-                </div>
-                <ul>
-                    <li className="itemAppointmentClient flex flex-col mb-5">
-                        <div className="itemHeader flex justify-between items-center p-md p-5">
-                            <strong>20 de setembro</strong>
-                            <span>08h-08h30</span>
+            <div>
+                <section className="pr-10 pl-20">
+                    <div className="flex flex-col items-start pt-20 pb-10">
+                        <h2 className="title-lg">Seus agendamentos</h2>
+                        <p className="p-sm subt-color">Aqui você pode visualizar seus horários marcados.</p>
+                    </div>
+                    <ul>
+                        <li className="itemAppointmentClient flex flex-col mb-5">
+                            <div className="itemHeader flex justify-between items-center p-md p-5">
+                                <strong>20 de setembro</strong>
+                                <span>08h-08h30</span>
+                            </div>
+                            <div className="itemData p-md pt-10 pb-10 pl-7 pr-7">
+                                <strong>15:00</strong>
+                                <span className="">Dr. Silva</span>
+                                <span>Cabelo</span>
+                                <strong className="flex justify-end">X</strong>
+                            </div>
+                        </li>
+                    </ul>
+                </section>
+                <section className="pr-10 pl-20">
+                    <div className="flex justify-between items-end pt-20 pb-10">
+                        <div className="flex flex-col items-start">
+                            <h2 className="title-lg">Seu histórico</h2>
+                            <p className="p-sm subt-color">Aqui você pode visualizar seus atendimentos passados.</p>
                         </div>
-                        <div className="itemData p-md pt-10 pb-10 pl-7 pr-7">
-                            <strong>15:00</strong>
-                            <span className="">Dr. Silva</span>
-                            <span>Cabelo</span>
-                            <strong className="flex justify-end">X</strong>
-                        </div>
-                    </li>
-                </ul>
-            </section>
-            <section className="pr-10 pl-20">
-                <div className="flex flex-col items-start pt-5 pb-10">
-                    <h2 className="title-lg">Seus agendamentos</h2>
-                    <p className="p-sm subt-color">Aqui você pode visualizar seus horários marcados.</p>
-                </div>
-                <ul>
-                    <li className="itemHistoricClient flex flex-col mb-5">
-                        <div className="itemHeader flex justify-between items-center p-md p-5">
-                            <strong>20 de setembro</strong>
-                            <span>08h-08h30</span>
-                        </div>
-                        <div className="itemData p-md pt-10 pb-10 pl-7 pr-7">
-                            <strong>15:00</strong>
-                            <span className="">Dr. Silva</span>
-                            <span>Cabelo</span>
-                        </div>
-                    </li>
-                </ul>
-            </section>
-        </div>
+                        <input type="date" name="appointmentDate" id="appointmentDate" />
+                    </div>
+                    <ul>
+                        <li className="itemHistoricClient flex flex-col mb-5">
+                            <div className="itemHeader flex justify-between items-center p-md p-5">
+                                <strong>20 de setembro</strong>
+                                <span>08h-08h30</span>
+                            </div>
+                            <div className="itemData p-md pt-10 pb-10 pl-7 pr-7">
+                                <strong>15:00</strong>
+                                <span className="">Dr. Silva</span>
+                                <span>Cabelo</span>
+                            </div>
+                        </li>
+                    </ul>
+                </section>
+            </div>
         );
-    }else if (role === "barber" || role === "admin") {
-        return (   
+    } else if (role === "barber" || role === "admin") {
+        return (
             <section className="pr-10 pl-20">
-                <div className="flex flex-col items-start pt-20 pb-10">
-                    <h2 className="title-lg">{role === "barber" ? "Seus atendimentos" : "Agendamentos"}</h2>
-                    <p className="p-sm subt-color">Aqui você pode visualizar os horários marcados.</p>
+                <div className="flex justify-between items-end pt-20 pb-10">
+                    <div className="flex flex-col items-start">
+                        <h2 className="title-lg">{role === "barber" ? "Seus atendimentos" : "Agendamentos"}</h2>
+                        <p className="p-sm subt-color">Aqui você pode visualizar os horários marcados.</p>
+                    </div>
+                    <input type="date" name="appointmentDate" id="appointmentDate" />
                 </div>
                 <ul>
-                    <li className="itemAppointmentBarber flex flex-col mb-5">
+                    <li className={(role === "barber" ? "itemAppointmentBarber" : "itemAppointmentAdmin") + " flex flex-col mb-5 pb-10"}>
                         <div className="itemHeader flex justify-between items-center p-md p-5">
-                            <strong>20 de setembro</strong>
+                            <span>
+                                <img src="/icon/morning.svg" alt="Print" className="inline-block w-6 h-6 mr-2" />
+                                Manhã
+                            </span>
                             <span>08h-08h30</span>
                         </div>
-                        <div className="itemData p-md pt-10 pb-10 pl-7 pr-7">
+                        <div className="itemData p-md pt-10 pl-7 pr-7">
                             <strong>15:00</strong>
                             <span className="">Dr. Silva</span>
                             <span>Cabelo</span>
-                            <strong className="flex justify-end">X</strong>
+                            {role === "admin" && (<>
+                                <span>BaFulanos</span>
+                                <strong className="close flex justify-end">X</strong>
+                            </>
+                            )}
+
+                        </div>
+                    </li>
+                    <li className={(role === "barber" ? "itemAppointmentBarber" : "itemAppointmentAdmin") + " flex flex-col mb-5 pb-10"}>
+                        <div className="itemHeader flex justify-between items-center p-md p-5">
+                            <span>
+                                <img src="/icon/afternoon.svg" alt="Print" className="inline-block w-6 h-6 mr-2" />
+                                Tarde
+                            </span>
+                            <span>08h-08h30</span>
+                        </div>
+                        <div className="itemData p-md pt-10 pl-7 pr-7">
+                            <strong>15:00</strong>
+                            <span className="">Dr. Silva</span>
+                            <span>Cabelo</span>
+                            {role === "admin" && (<>
+                                <span>BaFulanos</span>
+                                <strong className="close flex justify-end">X</strong>
+                            </>
+                            )}
+
+                        </div>
+                        <div className="itemData p-md pt-10 pl-7 pr-7">
+                            <strong>15:00</strong>
+                            <span className="">Dr. Silva</span>
+                            <span>Cabelo</span>
+                            {role === "admin" && (<>
+                                <span>BaFulanos</span>
+                                <strong className="close flex justify-end">X</strong>
+                            </>
+                            )}
+
+                        </div>
+                        <div className="itemData p-md pt-10 pl-7 pr-7">
+                            <strong>15:00</strong>
+                            <span className="">Dr. Silva</span>
+                            <span>Cabelo</span>
+                            {role === "admin" && (<>
+                                <span>BaFulanos</span>
+                                <strong className="close flex justify-end">X</strong>
+                            </>
+                            )}
+
+                        </div>
+                    </li>
+                    <li className={(role === "barber" ? "itemAppointmentBarber" : "itemAppointmentAdmin") + " flex flex-col mb-5 pb-10"}>
+                        <div className="itemHeader flex justify-between items-center p-md p-5">
+                            <span>
+                                <img src="/icon/night.svg" alt="Print" className="inline-block w-6 h-6 mr-2" />
+                                Noite
+                            </span>
+                            <span>08h-08h30</span>
+                        </div>
+                        <div className="itemData p-md pt-10 pl-7 pr-7">
+                            <strong>15:00</strong>
+                            <span className="">Dr. Silva</span>
+                            <span>Cabelo</span>
+                            {role === "admin" && (<>
+                                <span>BaFulanos</span>
+                                <strong className="close flex justify-end">X</strong>
+                            </>
+                            )}
+
                         </div>
                     </li>
                 </ul>
@@ -69,5 +144,6 @@ const ListAppointment = ({ role }: { role: string }) => {
         );
     }
 }
+
 
 export default ListAppointment;
