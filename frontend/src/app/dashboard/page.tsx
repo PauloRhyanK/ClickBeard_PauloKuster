0e4b531ca848f0fe8bfb704517737c5f7c70dde0
Paulo Kuster <paulorhyank@gmail.com>
2025-07-23 23:13:04 -0300

    fix: bug acess localstorage

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -10,31 +10,32 @@ export default function Dashboard() {
     const [token, setToken] = useState<string | null>(null);
     const [user, setUser] = useState<string | null>(null);
     const [role, setRole] = useState<string | null>(null);
+    const [isReady, setIsReady] = useState(false);
 
     useEffect(() => {
         setToken(localStorage.getItem("token"));
         setUser(localStorage.getItem("user"));
         setRole(localStorage.getItem("role"));
+        setIsReady(true);
     }, []);
 
     useEffect(() => {
-        async function checkAuth() {
+        if (isReady) {
             if (!token || !user || !role) {
                 Router.push('/login');
-                return;
             }
         }
-        checkAuth();
-    }, [token, user, role]);
+    }, [token, user, role, isReady]);
+
+    if (!isReady) return null; 
 
     return (
         <>
             <img className="absolute w-50 h-13 top-2 left-2" src="/clickbeader_logo.webp" alt="ClickBeader Logo" />
             <div className="dashboardContainer">
-                <NewAppointment role={role??""} />
-                <ListAppointment role={role??""} />
+                <NewAppointment role={role ?? ""} />
+                <ListAppointment role={role ?? ""} />
             </div>
         </>
-    )
-
+    );
 }
